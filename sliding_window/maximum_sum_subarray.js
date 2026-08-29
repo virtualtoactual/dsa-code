d2b66d913e6333488eac53a919fec9cd9a1cedb7
Tej Pratap <bankebihari5463@gmail.com>
2026-08-29 19:00:13 +0530

    done

--- sliding_window/maximum_sum_subarray.js
+++ sliding_window/maximum_sum_subarray.js
@@ -8,13 +8,20 @@ function maxSum(arr, k) {
     let left = 0;
     let right = 0;
     while (right < arr.length) {
+
+        //calcuation
         windowSum += arr[right];
+
+        //window size is not equal to k
         if (right - left + 1 < k) {
             right++;
         }
+
+        //window size is equal to k find the answer and slide the window
         else if (right - left + 1 === k) {
             maxSum = Math.max(maxSum, windowSum);
             windowSum -= arr[left];
+            //slide the window
             left++;
             right++;
         }
